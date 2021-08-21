609826d7f532717a5bca2c4176f07dcc5b393780
TTepel7 <55439124+TTepel7@users.noreply.github.com>
2021-08-22 04:15:40 +0500

    add download png

--- src/resources/js/components/ToolBar.js
+++ src/resources/js/components/ToolBar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import WorkArea from "./WorkArea";
-
-import { Switch, RadioGroup, RadioButton, Button } from '@design-system-rt/rtk-ui-kit';
-
+import html2canvas from 'html2canvas';
+import download from 'downloadjs'
+import {Switch, RadioGroup, RadioButton, Button} from '@design-system-rt/rtk-ui-kit';
 
 
 function ToolBar() {
@@ -12,24 +12,37 @@ function ToolBar() {
         text: 'Проверка'
     });
 
-    function get_input_change(value) {
-        setOption({ text: value })
+    function export_work() {
+        let canvas= html2canvas(document.querySelector(".work-area"),{ useCORS: true}).then(canvas => {
+                let img=canvas.toDataURL("image/png");
+             download(img,'test_img',"image/png")
+        });
+
+
+
     }
+
+    function get_input_change(value) {
+        setOption({text: value})
+    }
+
     return (
-        <div className="tool-bar" >
+        <div className="tool-bar">
             <Switch
                 color="primary2"
                 defaultChecked
-                onChange={function noRefCheck() { }}
+                onChange={function noRefCheck() {
+                }}
                 shape="circular"
                 text="Тёмная тема"
                 textPosition="right"
                 textStyle={{
-                    paddingRight:'1rem'
+                    paddingRight: '1rem'
                 }}
             />
             <RadioGroup
-                onChange={function noRefCheck() { }}
+                onChange={function noRefCheck() {
+                }}
                 value="key1"
                 style={{
                     display: 'inline'
@@ -39,8 +52,8 @@ function ToolBar() {
                     name="rb1"
                     value="key1"
                     style={{
-                        top:'-0.75rem',
-                        paddingRight:'1rem'
+                        top: '-0.75rem',
+                        paddingRight: '1rem'
                     }}
                 >
                     Геометрический
@@ -50,8 +63,8 @@ function ToolBar() {
                     name="rb2"
                     value="key2"
                     style={{
-                        top:'-0.75rem',
-                        paddingRight:'1rem'
+                        top: '-0.75rem',
+                        paddingRight: '1rem'
                     }}
                 >
                     Скруглённый
@@ -60,14 +73,16 @@ function ToolBar() {
                     name="rb3"
                     value="key3"
                     style={{
-                        top:'-0.75rem',
-                        paddingRight:'1rem'
+                        top: '-0.75rem',
+                        paddingRight: '1rem'
                     }}
                 >
                     Круглый
                 </RadioButton>
             </RadioGroup>
-            <Button style={{ float: "right", paddingRight:"1rem",marginLeft: 'auto', marginTop: '-0.75rem'}} shape="circular">Экспортировать</Button>
+            <Button onClick={export_work}
+                    style={{float: "right", paddingRight: "1rem", marginLeft: 'auto', marginTop: '-0.75rem'}}
+                    shape="circular">Экспортировать</Button>
         </div>
     );
 }
